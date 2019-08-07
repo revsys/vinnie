320a3d200fcfc6be48c0be34c1ee22e62655e0da
Frank Wiles <frank@revsys.com>
2019-08-07 16:34:02 -0500

    Increase version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ f.close()
 
 setup(
     name="vinnie",
-    version="0.5.0",
+    version="0.5.1",
     py_modules=["vinnie"],
     install_requires=["Click==7.0", "semver==2.8.1", "GitPython==2.1.11"],
     tests_require=["pytest==5.0.1", "pytest-sugar==0.9.2", "pytest-cov==2.7.1"],
