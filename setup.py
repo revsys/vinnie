84f8f2f5d67e84242124e314817d21a0f3c1a804
Frank Wiles <frank@revsys.com>
2019-11-13 15:57:37 -0600

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ f.close()
 
 setup(
     name="vinnie",
-    version="0.5.2",
+    version="0.6.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=["Click==7.0", "semver==2.8.1", "GitPython==2.1.11"],
     tests_require=["pytest==5.0.1", "pytest-sugar==0.9.2", "pytest-cov==2.7.1"],
